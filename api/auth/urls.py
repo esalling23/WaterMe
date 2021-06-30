2a060000872a9b0287e7f887ceeee9d2b3fe2bbc
esalling23 <esalling23@gmail.com>
2021-06-30 17:53:54 -0400

    V1 Files for API with user auth - login, sign up, signout, change password

---
+++ api/auth/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import SignUp, SignIn, SignOut, ChangePassword
+
+urlpatterns = [
+    path('sign-up/', SignUp.as_view(), name='sign-up'),
+    path('sign-in/', SignIn.as_view(), name='sign-in'),
+    path('sign-out/', SignOut.as_view(), name='sign-out'),
+    path('change-pw/', ChangePassword.as_view(), name='change-pw')
+]
